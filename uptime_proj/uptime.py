145e892d7483124a339f62fa92459d46127f778b
Zabi Mukthar <zabi_ahmed@apple.com>
2019-07-07 15:39:26 -0700

    new projects

---
+++ uptime_proj/uptime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import subprocess
+from flask import Flask
+
+app=Flask(__name__)
+
+
+@app.route('/uptime')
+def uptime():
+	open_pipe=subprocess.Popen("uptime", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	output,err=open_pipe.communicate()
+	rc=open_pipe.wait()
+	return output.strip()
+
+if __name__ == '__main__':
+	app.run(debug=True, port=10500) 
